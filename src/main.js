44c240cbc6af8f376812575c987a46486cdb5a6b
Ramses <ramses.mtz96@gmail.com>
2020-11-22 11:24:56 -0600

    Commit inicial

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
+// Tailwinwd
+import "tailwindcss/tailwind.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
